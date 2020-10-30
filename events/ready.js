e3a52d284f95f7e5bbaa92521eeaa3afef46089b
fire0001 <72525097+fire0001@users.noreply.github.com>
2020-10-30 20:44:33 +0600

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,8 @@
+module.exports = (client) => {
+    console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`)
+let totalUsers = client.guilds.cache.reduce((acc, value) => acc + value.memberCount, 0)
+    var activities = [ `${client.guilds.cache.size} servers`, `${totalUsers} users!` ], i = 0;
+    setInterval(() => client.user.setActivity(`g!start | ${activities[i++ % activities.length]}`, { type: "WATCHING" }),3000)
+    
+
+};
