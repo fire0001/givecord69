9c01b41b38e36cd68e5c2f3e97fb54c683878e3d
fire0001 <72525097+fire0001@users.noreply.github.com>
2020-11-02 02:03:18 +0600

    Added Mention to show Prefix .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const db = require('quick.db');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -20,13 +20,22 @@ client.giveawaysManager = new GiveawaysManager(client, {
 // We now have a client.giveawaysManager property to manage our giveaways!
 
 client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
-    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`) 
-member.send(`Your Giveaway enter has been confirmed !!`).catch(() => console.log(`Failed to DM ` + member.user.tag));
+    console.log(`${member.user.tag} entered giveaway ${giveaway.guildID}`) 
+const em = new Discord.MessageEmbed()
+.setColor(`GREEN`)
+.setTitle(`:mailbox_with_mail: You have successfully entered the giveaway ! :postbox:`)
+.setDescription(`Your Giveaway entry for ${giveaway.prize} has been confirmed! Do not leave the server otherwise your giveaway entry will denied! \n[Giveaway Link](https://discord.com/channels/${giveaway.guildID}/${giveaway.channelID}/${giveaway.messageID})`)
+member.send(em).catch(() => console.log(`Failed to DM ` + member.user.tag));
 });
 
-client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
-    console.log(`${member.user.tag} unreact to giveaway ${giveaway.guild}  (${reaction.emoji.name})`); 
-   member.send(`Giveaway Denied`).catch(() => console.log("Failed to DM "+ member.user.tag))
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, guild, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway in ${giveaway.guildID}`); 
+const emd = new Discord.MessageEmbed()
+.setColor(`RED`)
+.setTitle(`:mailbox_with_mail: Your giveaway entry was denied! :postbox:`)
+.setDescription(`Your Giveaway entry for ${giveaway.prize} has been denied! You may unreact or left the server! If you think its a mistake then refresh you discord and react again! \n[Giveaway Link](https://discord.com/channels/${giveaway.guildID}/${giveaway.channelID}/${giveaway.messageID})`)
+.setFooter(`Invite **Giv'Cord** in your server! Type g!invite `) 
+   member.send(emd).catch(() => console.log("Failed to DM "+ member.user.tag))
 });
 
 /* Load all events */
@@ -54,7 +63,18 @@ fs.readdir("./commands/", (_err, files) => {
     });
 });
 
-/* Load all Owner CMDs */
+/* Load all commands */
+fs.readdir("./mod/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./mod/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+        console.log(`Moderation CMD loaded: ${commandName}`);
+    });
+});
+
+/* Load all commands */
 fs.readdir("./owner/", (_err, files) => {
     files.forEach((file) => {
         if (!file.endsWith(".js")) return;
@@ -64,6 +84,29 @@ fs.readdir("./owner/", (_err, files) => {
         console.log(`Owner CMD loaded: ${commandName}`);
     });
 });
+
+/* Load all commands */
+fs.readdir("./info/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./info/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+        console.log(`Info CMD loaded: ${commandName}`);
+    });
+});
+
+
+
+/* if Someone Mention the bot  !!!*/
+
+client.on('message', async message => {
+
+        if (message.mentions.has(client.user) && !message.mentions.has(message.guild.id)) {
+            return message.channel.send(`My Prefix is - \`g!\` . Type \`g!help\` to see a list of commands!`)
+        }
+    } 
+) 
 
 // Login
 client.login(config.token);
