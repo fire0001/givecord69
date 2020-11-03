3c0d62c29476e1a0738f38e676a415493baab403
fire0001 <72525097+fire0001@users.noreply.github.com>
2020-11-04 04:41:46 +0600

    updated Timestamp

--- functions.js
+++ functions.js
@@ -30,8 +30,28 @@ module.exports = {
     return new Promise(resolve => setTimeout(resolve, ms));
   },
 
-  formatDate: function (date) {
-    return new Intl.DateTimeFormat('en-US').format(date);
+  formatDate: function dateToNiceString(myDate){
+    var month=new Array();
+  month[0]="Jan";
+  month[1]="Feb";
+  month[2]="Mar";
+  month[3]="Apr";
+  month[4]="May";
+  month[5]="Jun";
+  month[6]="Jul";
+  month[7]="Aug";
+  month[8]="Sep";
+  month[9]="Oct";
+  month[10]="Nov";
+  month[11]="Dec";
+  var hours = myDate.getHours();
+  var minutes = myDate.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ampm;
+    return month[myDate.getMonth()]+" "+myDate.getDate()+" , "+myDate.getFullYear()+" | "+strTime;
   },
 
   promptMessage: async function (message, author, time, validReactions) {
